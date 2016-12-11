44fd0ecda5093ee0b201ebed1a5ebd0456540e09
Magno Gouveia <owflee@gmail.com>
2016-12-11 19:13:37 -0200

    Added string to waves

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -14,11 +15,19 @@ public class GameManager : MonoBehaviour {
     public int enemyPerTurn;
     public int minInterval;
     public int maxInterval;
+    public int vidas;
+    public int blocksDinheiro;
     public EnemySpawn spawn;
     public Room room;
 
+    public Text waves;
+    public Text blocksText;
+    public Text lifeText;
+
+
     // Use this for initialization
     void Start () {
+
         this.actualTurn = 0;
         this.turnActive = false;
         this.activeWeapon = 2;
@@ -27,6 +36,9 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        waves.text = "WAVE " + this.actualTurn;
+        blocksText.text = ""+this.blocksDinheiro;
+        lifeText.text = ""+this.vidas;
         if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0 && this.turnActive) {
             this.stopTurn();
         };
