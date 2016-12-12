294648787a3eeeaf4de760c5d2110cb80b949e84
Magno Gouveia <owflee@gmail.com>
2016-12-12 09:56:21 -0200

    Added options to menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    public void buttonClick(string button)
+    {
+        switch (button) {
+            case "start":
+                SceneManager.LoadScene("MainGame");
+                break;
+            case "options":
+                SceneManager.LoadScene("Options");
+                break;
+            case "quit":
+                Application.Quit();
+                break;
+            case "credits":
+                SceneManager.LoadScene("Credits");
+                break;
+            default:
+                break;            
+        }
+
+    }
+
+}
